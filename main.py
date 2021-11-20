b7077fe79288341a10c1dfaff3acb646370c29cc
Attioss <attioss13@gmai.com>
2021-11-20 12:43:38 +0100

    End of the project

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import os
+import time
+import datetime
 
+# A program bizonyos idokozonkent, kinyeri a waze alkalmazasbol, az aktualis utazasi idot.
 def web_scraping():
-    page = requests.get('https://www.waze.com/en/live-map/directions/tokol-magyarorszag?utm_source=website&utm_medium=homepage&utm_campaign=iframe+module&to=place.ChIJHaMnPRvlQUcR8CYeDCnEAAQ&from=place.ChIJC0BjZOzdQUcR4NUeDCnEAAU')
-    soup = BeautifulSoup(page.content, 'html.parser')
-    week = soup.find(Class='wm-routes multiple-routes')
-    print(week)
-
-
-
-
+    count = 0
+    os.environ['PATH'] += r"C:/SeleniumDrivers"
+    while count < 96:
+        driver = webdriver.Chrome()
+        driver.get('https://www.waze.com/en/live-map/directions/tokol-magyarorszag?utm_source=website&utm_medium=\
+        homepage&utm_campaign=iframe+module&to=place.ChIJHaMnPRvlQUcR8CYeDCnEAAQ&from=place.ChIJC0BjZOzdQUcR4NUeDCnEAAU')
+        time.sleep(4)  # eleg 4 is, fejlesztesnek lehet az ido annyi, amennyi ido alatt megnyilik rendesen az oldal
+        my_element = driver.find_element(By.CSS_SELECTOR, ".wm-routes-item-desktop.is-active")
+        my_class = my_element.find_element(By.CLASS_NAME, "wm-routes-item-desktop__header")
+        my_path = my_class.find_elements(By.XPATH, "./span")
+        my_span = my_path[1].find_elements(By.XPATH, "./span")
+        result = my_span[0].get_attribute('innerHTML')  # divben 'text', spanben 'innerHTML'
+        res_time = datetime.datetime.now().time()
+        print(result)
+        file_name = r'D:\my_text\my_text.txt'
+        with open(file_name, 'a') as x_file:
+            x_file.write(str(res_time) + ' ')
+            x_file.write(result + '\n')
+        count += 1
+        driver.close()
+        time.sleep(900)
 
 
 
